1180dc8f11c1dd5411bfab14fbe259a8fdb78435
YoussefCydeo <Youssefbouzoubaa1996@gmail.com>
2023-07-17 15:46:55 -0400

    Updated my project

--- src/test/java/com/B29_GR17_Project/runners/CukesRunner.java
+++ src/test/java/com/B29_GR17_Project/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/B29_GR17_Project/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "",
         publish = true //generating a report with public link
 )
