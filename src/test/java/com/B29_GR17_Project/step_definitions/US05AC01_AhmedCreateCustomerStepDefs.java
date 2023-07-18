556c2cb374b8fd7fccf8350a42ce222f096d4ed7
Ahmed Bouzoubaa <ahmedbouzou95@gmail.com>
2023-07-17 22:58:24 -0400

    name change to show AC01

--- src/test/java/com/B29_GR17_Project/step_definitions/US05AC01_AhmedCreateCustomerStepDefs.java
+++ src/test/java/com/B29_GR17_Project/step_definitions/US05AC01_AhmedCreateCustomerStepDefs.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class US05_AhmedCreateCustomerStepDefs {
+public class US05AC01_AhmedCreateCustomerStepDefs {
     AhmedLoginPage loginPage = new AhmedLoginPage();
     AhmedHomePage homePage = new AhmedHomePage();
 
