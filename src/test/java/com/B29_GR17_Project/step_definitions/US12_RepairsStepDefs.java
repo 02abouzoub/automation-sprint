262477aed10feb73d72c004d0b348ced8f2aa74a
Israa_Cydeo <Israa.eltom@gmail.com>
2023-07-20 10:49:15 -0400

    Committing US12 test scripts

---
+++ src/test/java/com/B29_GR17_Project/step_definitions/US12_RepairsStepDefs.java
@@ -0,0 +1,63 @@
+package com.B29_GR17_Project.step_definitions;
+
+import com.B29_GR17_Project.pages.US12_RepairsPage_IA;
+import com.B29_GR17_Project.utilities.ConfigurationReader;
+import com.B29_GR17_Project.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class US12_RepairsStepDefs {
+
+    US12_RepairsPage_IA repairsPage = new US12_RepairsPage_IA();
+    @Given("user in the login page")
+    public void user_in_the_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+    }
+    @When("the user login with valid {string},{string}")
+    public void theUserLoginWithValid(String email, String password) {
+        repairsPage.loginAndClick(email,password);
+    }
+
+    @And("user clicks on the repair button in the dashboard")
+    public void userClicksOnTheRepairButtonInTheDashboard() {
+        WebDriverWait wait= new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.elementToBeClickable(repairsPage.repairsPageButton) );
+
+
+        repairsPage.repairsPageButton.click();
+
+    }
+
+
+
+    @And("user click on the first checkbox")
+    public void userClickOnTheFirstCheckbox() {
+        WebDriverWait wait= new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.elementToBeClickable(repairsPage.topRepairOrdersCheckBox) );
+
+        repairsPage.topRepairOrdersCheckBox.click();
+
+    }
+
+    @Then("user is able to select all repair orders")
+    public void userIsAbleToSelectAllRepairOrders() {
+        List<WebElement> actual= repairsPage.allRepairOrders;
+        boolean assertion= false;
+        for (WebElement each: actual) {
+            assertion= each.isSelected();
+        }
+
+        Assert.assertTrue(assertion);
+
+    }
+}
