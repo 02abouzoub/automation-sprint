242df765fd35e9a8ee3090cdb8a37eff2dcde2e4
Ahmed Bouzoubaa <ahmedbouzou95@gmail.com>
2023-07-21 10:30:13 -0400

    US06-Hooks issue fixed

--- src/test/java/com/B29_GR17_Project/step_definitions/Hooks.java
+++ src/test/java/com/B29_GR17_Project/step_definitions/Hooks.java
@@ -1,3 +1,5 @@
+package com.B29_GR17_Project.step_definitions;
+
 import com.B29_GR17_Project.utilities.BrowserUtils;
 import com.B29_GR17_Project.utilities.ConfigurationReader;
 import com.B29_GR17_Project.utilities.Driver;
