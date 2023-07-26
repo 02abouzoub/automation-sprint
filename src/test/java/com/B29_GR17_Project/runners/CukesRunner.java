085018d5d237dfcd4caf7a6ca8b02167ec77b918
Ahmed Bouzoubaa <ahmedbouzou95@gmail.com>
2023-07-25 20:02:49 -0400

    fixed cukesRunner

--- src/test/java/com/B29_GR17_Project/runners/CukesRunner.java
+++ src/test/java/com/B29_GR17_Project/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/B29_GR17_Project/step_definitions",
         dryRun = false,
-        tags = "@B29G17-266-Regression",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
